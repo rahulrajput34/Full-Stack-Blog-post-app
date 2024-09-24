c4e17cf4eff6333976fa87164b352bf3746d1d29
Rahul Rajput <rahul242rajput@gmail.com>
2024-09-24 13:00:30 -0400

    Second time commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo() {
   return (
     <div>
-        <img src="logo.webp" alt="logo" className='rounded-full h-10 w-10'/>
+        <img src="https://images.pexels.com/photos/430205/pexels-photo-430205.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="logo" className='rounded-full h-10 w-10'/>
     </div>
   )
 }
