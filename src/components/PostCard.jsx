e2d671a3da3ebde628cd656aa7c4c4eadbcebfab
Rahul Rajput <rahul242rajput@gmail.com>
2024-09-24 21:13:05 -0400

    change post card for image and give padding to login

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import appwriteService from "../appwrite/config"
-import {Link} from 'react-router-dom'
+import React from 'react';
+import appwriteService from "../appwrite/config";
+import { Link } from 'react-router-dom';
 
-function PostCard({$id, title, featuredImage}) {
-    
-  return (
-    <Link to={`/post/${$id}`}>
-        <div className='w-full bg-purple-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl' />
+function PostCard({ $id, title, featuredImage }) {
+    const imageUrl = appwriteService.getFilePreview(featuredImage);
 
+    return (
+        <Link to={`/post/${$id}`}>
+            <div className='w-full bg-purple-100 rounded-xl p-4'>
+                <div className='w-full justify-center mb-4'>
+                    {imageUrl ? (
+                        <img 
+                            src={imageUrl} 
+                            alt={title} 
+                            className='rounded-xl' 
+                        />
+                    ) : (
+                        <div className='flex items-center justify-center h-40 bg-gray-200 rounded-xl'>
+                            <span>Unsupported format</span>
+                        </div>
+                    )}
+                </div>
+                <h2 className='text-xl font-bold'>{title}</h2>
             </div>
-            <h2
-            className='text-xl font-bold'
-            >{title}</h2>
-        </div>
-    </Link>
-  )
+        </Link>
+    );
 }
 
-
-export default PostCard
+export default PostCard;
