0fb67fb76ca220cc6d2724ba7f28c0f0e4d42bd4
Rahul Rajput <rajputrahul3424@gmail.com>
2025-08-20 17:23:58 -0400

    fix: add the dynamic logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import logo from "../assets/logo.png";
+
 
 function Logo() {
   return (
     <div>
-        <img src="https://images.pexels.com/photos/430205/pexels-photo-430205.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="logo" className='rounded-full h-10 w-10'/>
+      <img src={logo} alt="logo" className="rounded-full h-10 w-10" />
     </div>
-  )
+  );
 }
 
 export default Logo
