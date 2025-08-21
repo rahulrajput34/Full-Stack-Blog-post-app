5faeb0525724c141c4cdb2e9566c85c46d791e7d
Rahul Rajput <rajputrahul3424@gmail.com>
2025-08-21 14:06:00 -0400

    fix:  make  the header content professional

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,8 +17,7 @@ function Header() {
 
   const navItems = [
     { name: "Home", slug: "/", active: true },
-    { name: "Login", slug: "/login", active: !authStatus },
-    { name: "Signup", slug: "/signup", active: !authStatus },
+    { name: "Account", slug: "/login", active: !authStatus },
     { name: "All Posts", slug: "/all-posts", active: authStatus },
     { name: "Add Post", slug: "/add-post", active: authStatus },
   ];
